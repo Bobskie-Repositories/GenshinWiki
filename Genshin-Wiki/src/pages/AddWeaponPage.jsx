a199a8b55df261b0c156c503e7707c6e24d55d19
Bob Kyle L. Rosales <bobkyle.rosales@cit.edu>
2024-05-14 17:19:59 +0800

    create pages for artifact and weapon

--- Genshin-Wiki/src/pages/AddWeaponPage.jsx
+++ Genshin-Wiki/src/pages/AddWeaponPage.jsx
@@ -3,23 +3,21 @@ import { useNavigate, Link } from "react-router-dom";
 import { FaArrowLeft } from "react-icons/fa";
 import apiConfig from "../config";
 
-const AddCharacterPage = () => {
+const AddWeaponPage = () => {
   const navigate = useNavigate();
-  const [characterInfo, setCharacterInfo] = useState({
+  const [weaponInfo, setWeaponInfo] = useState({
     name: "",
-    description: "",
     rarity: "",
-    vision: "",
-    weapon: "",
+    type: "",
+    atk: "",
     obtain: "",
-    birthday: "",
     image_url: "",
   });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setCharacterInfo({
-      ...characterInfo,
+    setWeaponInfo({
+      ...weaponInfo,
       [name]: value,
     });
   };
@@ -27,27 +25,25 @@ const AddCharacterPage = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch(`${apiConfig.API_URL}/characters`, {
+      const response = await fetch(`${apiConfig.API_URL}/weapons`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(characterInfo),
+        body: JSON.stringify(weaponInfo),
       });
       if (!response.ok) {
-        throw new Error("Failed to save character");
+        throw new Error("Failed to save weapon");
       }
-      setCharacterInfo({
+      setWeaponInfo({
         name: "",
-        description: "",
         rarity: "",
-        vision: "",
-        weapon: "",
+        type: "",
+        atk: "",
         obtain: "",
-        birthday: "",
         image_url: "",
       });
-      navigate("/characters");
+      navigate("/weapons");
     } catch (error) {
       console.error("Error:", error);
     }
@@ -56,10 +52,10 @@ const AddCharacterPage = () => {
   return (
     <div className="flex justify-center">
       <div className="bg-slate-100 w-9/12 bg-opacity-90 py-8 px-10 my-10 rounded-lg">
-        <Link to="/characters">
+        <Link to="/weapons">
           <FaArrowLeft />
         </Link>
-        <span className="font-bold m-8 sm:text-xl md:text-3xl ">Add Character</span>
+        <span className="font-bold m-8 sm:text-xl md:text-3xl">Add Weapon</span>
         <div className="border border-yellow-500 px-8 pb-8 pt-4 m-8 rounded-md">
           <form onSubmit={handleSubmit}>
             <div className="mb-4">
@@ -70,58 +66,13 @@ const AddCharacterPage = () => {
                 type="text"
                 id="name"
                 name="name"
-                value={characterInfo.name}
+                value={weaponInfo.name}
                 onChange={handleChange}
                 className="border border-gray-400 rounded-md py-2 px-3 w-full"
                 required
               />
             </div>
-            <div className="mb-4">
-              <label htmlFor="description" className="block font-semibold mb-2">
-                Description:
-              </label>
-              <textarea
-                id="description"
-                name="description"
-                value={characterInfo.description}
-                onChange={handleChange}
-                className="border border-gray-400 rounded-md py-2 px-3 w-full h-24"
-                required
-              />
-            </div>
-
             <div className="flex gap-8 items-center justify-center">
-              <div className="mb-4">
-                <label htmlFor="gender" className="block font-semibold mb-2">
-                  Gender:
-                </label>
-                <select
-                  id="gender"
-                  name="gender"
-                  value={characterInfo.gender}
-                  onChange={handleChange}
-                  className="border border-gray-400 rounded-md py-2 px-3 w-full"
-                  required
-                >
-                  <option value="">Select Gender</option>
-                  <option value="male">Male</option>
-                  <option value="female">Female</option>
-                </select>
-              </div>
-              <div className="mb-4">
-                <label htmlFor="birthday" className="block font-semibold mb-2">
-                  Birthday:
-                </label>
-                <input
-                  type="text"
-                  id="birthday"
-                  name="birthday"
-                  value={characterInfo.birthday}
-                  onChange={handleChange}
-                  className="border border-gray-400 rounded-md py-2 px-3 w-full"
-                  required
-                />
-              </div>
               <div className="mb-4">
                 <label htmlFor="rarity" className="block font-semibold mb-2">
                   Rarity:
@@ -129,7 +80,7 @@ const AddCharacterPage = () => {
                 <select
                   id="rarity"
                   name="rarity"
-                  value={characterInfo.rarity}
+                  value={weaponInfo.rarity}
                   onChange={handleChange}
                   className="border border-gray-400 rounded-md py-2 px-3 w-full"
                   required
@@ -140,46 +91,38 @@ const AddCharacterPage = () => {
                 </select>
               </div>
               <div className="mb-4">
-                <label htmlFor="vision" className="block font-semibold mb-2">
-                  Vision:
+                <label htmlFor="type" className="block font-semibold mb-2">
+                  Type:
                 </label>
                 <select
-                  id="vision"
-                  name="vision"
-                  value={characterInfo.vision}
+                  id="type"
+                  name="type"
+                  value={weaponInfo.type}
                   onChange={handleChange}
                   className="border border-gray-400 rounded-md py-2 px-3 w-full"
                   required
                 >
-                  <option value="">Select vision</option>
-                  <option value="geo">Geo</option>
-                  <option value="pyro">Pyro</option>
-                  <option value="anemo">Anemo</option>
-                  <option value="cryo">Cryo</option>
-                  <option value="hydro">Hydro</option>
-                  <option value="electro">Electro</option>
-                  <option value="dendro">Dendro</option>
-                </select>
-              </div>
-              <div className="mb-4">
-                <label htmlFor="weapon" className="block font-semibold mb-2">
-                  Weapon:
-                </label>
-                <select
-                  id="weapon"
-                  name="weapon"
-                  value={characterInfo.weapon}
-                  onChange={handleChange}
-                  className="border border-gray-400 rounded-md py-2 px-3 w-full"
-                  required
-                >
-                  <option value="">Select weapon</option>
+                  <option value="">Select type</option>
                   <option value="bows">Bows</option>
                   <option value="polearms">Polearms</option>
                   <option value="sword">Sword</option>
                   <option value="claymore">Claymore</option>
                   <option value="catalyst">Catalyst</option>
                 </select>
+              </div>
+              <div className="mb-4">
+                <label htmlFor="atk" className="block font-semibold mb-2">
+                  Attack Dmg:
+                </label>
+                <input
+                  type="text"
+                  id="atk"
+                  name="atk"
+                  value={weaponInfo.atk}
+                  onChange={handleChange}
+                  className="border border-gray-400 rounded-md py-2 px-3 w-full"
+                  required
+                />
               </div>
             </div>
             <div className="mb-4">
@@ -190,7 +133,7 @@ const AddCharacterPage = () => {
                 type="text"
                 id="obtain"
                 name="obtain"
-                value={characterInfo.obtain}
+                value={weaponInfo.obtain}
                 onChange={handleChange}
                 className="border border-gray-400 rounded-md py-2 px-3 w-full"
                 required
@@ -204,13 +147,12 @@ const AddCharacterPage = () => {
                 type="text"
                 id="image_url"
                 name="image_url"
-                value={characterInfo.image_url}
+                value={weaponInfo.image_url}
                 onChange={handleChange}
                 className="border border-gray-400 rounded-md py-2 px-3 w-full"
                 required
               />
             </div>
-
             <div className="flex justify-center">
               <button
                 type="submit"
@@ -226,4 +168,4 @@ const AddCharacterPage = () => {
   );
 };
 
-export default AddCharacterPage;
+export default AddWeaponPage;
--- Genshin-Wiki/src/pages/WeaponsPage.jsx
+++ Genshin-Wiki/src/pages/WeaponsPage.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 import { useData } from "../context/DataContext";
 import WeaponsList from "../components/WeaponsList";
 
@@ -33,6 +34,11 @@ const WeaponsPage = () => {
     <div className="flex justify-center">
       <div className="bg-slate-100 w-9/12 bg-opacity-90 py-8 px-10 my-10 rounded-lg">
         <span className="font-bold sm:text-xl md:text-3xl">Weapons</span>
+        <div className="flex justify-end">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-2 px-4 mr-8 rounded">
+            <NavLink to="add">Add Weapons</NavLink>
+          </button>
+        </div>
         <div className="border border-yellow-500 px-8 pb-8 pt-4 m-8 rounded-md">
           <WeaponsList weapons={currentWeapons} />
           <div className="flex justify-center mt-4">{renderPaginationButtons()}</div>
