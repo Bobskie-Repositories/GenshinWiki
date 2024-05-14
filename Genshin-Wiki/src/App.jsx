e2b2ea770f5f2f66a78c0cf61f6e191ca82d8c24
Bob Kyle L. Rosales <bobkyle.rosales@cit.edu>
2024-05-14 11:24:40 +0800

    Add routes

--- Genshin-Wiki/src/App.jsx
+++ Genshin-Wiki/src/App.jsx
@@ -1,9 +1,25 @@
-import "./App.css";
+import { Route, Routes, Router } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+import CharactersPage from "./pages/CharactersPage";
+import WeaponsPage from "./pages/WeaponsPage";
+import ArtifactPage from "./pages/ArtifactPage";
+import backgroundImage from "/genshinBG.jpg";
 
 function App() {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <div
+      className="bg-cover bg-center min-h-screen"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="characters" element={<CharactersPage />} />
+          <Route path="weapons" element={<WeaponsPage />} />
+          <Route path="artifacts" element={<ArtifactPage />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
