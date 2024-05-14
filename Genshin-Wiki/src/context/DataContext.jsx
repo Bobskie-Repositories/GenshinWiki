dcf76a24126fb464b4a194cfd638460855ceaa76
Bob Kyle L. Rosales <bobkyle.rosales@cit.edu>
2024-05-14 16:37:06 +0800

    add apiconfig/config.js

--- Genshin-Wiki/src/context/DataContext.jsx
+++ Genshin-Wiki/src/context/DataContext.jsx
@@ -1,4 +1,5 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
+import apiConfig from "../config";
 
 const DataContext = createContext();
 
@@ -10,15 +11,15 @@ export const DataProvider = ({ children }) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const charactersResponse = await fetch("http://localhost:8000/characters");
+        const charactersResponse = await fetch(`${apiConfig.API_URL}/characters`);
         const charactersData = await charactersResponse.json();
         setCharacters(charactersData);
 
-        const weaponsResponse = await fetch("http://localhost:8000/weapons");
+        const weaponsResponse = await fetch(`${apiConfig.API_URL}/weapons`);
         const weaponsData = await weaponsResponse.json();
         setWeapons(weaponsData);
 
-        const artifactsResponse = await fetch("http://localhost:8000/artifacts");
+        const artifactsResponse = await fetch(`${apiConfig.API_URL}/artifacts`);
         const artifactsData = await artifactsResponse.json();
         setArtifacts(artifactsData);
       } catch (error) {
